b10c945521d59df38f41437b248ae51996c2792c
Hitesh Kumar Patel <itwasnoteasy@Hiteshs-MacBook-Pro.local>
2018-05-20 22:37:39 -0700

    Fetch real stocks from IEX REST API

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,7 @@ import { NavController } from 'ionic-angular';
 import { environment } from '@app/env';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Company } from 'app/model/company.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
@@ -10,17 +11,24 @@ import { Company } from 'app/model/company.model';
 })
 export class HomePage {
 
-  watchlist: Company[];
+  stockWeb: any;
+  watchlist: Company[] = [];
+  // 10 Tickers: Apple, Facebook, Microsoft, Netflix, 
+  // Google, Amazon, Qualcomm, Bank of America, Tesla, Adobe
+  tickers: string = 'aapl,adbe,amzn,bac,fb,goog,msft,nflx,qcom,tsla';
   constructor(public navCtrl: NavController,
-  public afDb: AngularFireDatabase) {}
+  public http: HttpClient) {}
 
   ngOnInit() {
     this.getWatchlist();
   }
 
   getWatchlist() {
-    this.afDb.list('/users/playstock/watchlist').valueChanges().subscribe((data: Company[]) => {
-      this.watchlist = data;
+    this.http.jsonp('https://api.iextrading.com/1.0/stock/market/quote?displayPercent=true&symbols='+this.tickers+'&filter=sector,previousClose,companyName,changePercent,latestPrice,open,symbol,','callback').subscribe((data:any[]) => {
+        data.forEach((value) => {
+          this.watchlist.push(value);
+        })
     });
+    
   }
 }
